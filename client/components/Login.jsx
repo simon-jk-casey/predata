a4f196331565a6450ef30103d59b2d09946b6fa4
simon-jk-casey <jesusiwasevil@gmail.com>
2017-07-07 13:12:12 +1200

    client login working

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+
+import { login } from '../api'
 import * as actions from '../actions/index'
 
 class Login extends React.Component {
+
+  handleSumbit (evt) {
+    evt.preventDefault()
+    login(this.props.loginDetails)
+    this.props.changePage('/profile')
+  }
+
   render () {
     return (
       <div>
-        <form id='userAuth'>
+        <form id='userAuth' onSubmit={(evt) => this.handleSumbit(evt)}>
           <label htmlFor='email'>Email Address: </label>
           <input type='text' name='email' onChange={(evt) => this.props.inputChange(evt)} />
           <label htmlFor='password'>Password: </label>
           <input type='password' name='password' onChange={(evt) => this.props.inputChange(evt)} />
         </form>
-        <button form='userAuth' onClick={() => this.props.changePage()}>Login</button>
+        <button form='userAuth' type='submit'>Login</button>
       </div>
     )
   }
@@ -25,7 +34,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
-  changePage: () => push('/addPredator'),
+  changePage: (route) => push(route),
   inputChange: (evt) => dispatch(actions.updateLoginDetails(evt.target.name, evt.target.value))
 }, dispatch)
 
